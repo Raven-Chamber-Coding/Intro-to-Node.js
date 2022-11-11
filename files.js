701c13af7ec9bad981c4cba45c270b07b60dae04
Lucas <90351520+Raven-Chamber-Coding@users.noreply.github.com>
2022-11-11 04:57:06 +0000

    Changes to files.js

--- files.js
+++ files.js
@@ -1,7 +1,14 @@
 const fs = require("fs");
 
 // reading files
-fs.readFile("")
+fs.readFile("./docs/blog1.txt", (err, data) => {
+  if (err) {
+    console.log(err);
+  }
+  console.log(data.toString());
+});
+
+console.log("last line");
 // writing files
 
 // directories
